b2fe3be5d51c48eeb49342eb65f8ee5c0831b89a
Sergey Zhukov <svg@ngs.ru>
2017-05-01 19:26:50 +0700

    Initial commit

---
+++ WixDir/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WixSharp;
+
+namespace WixDir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var project = new ManagedProject()
+            {
+                Dirs = new[]
+                {
+                    new Dir(@"%ProgramFiles%\WixDirSample", new Files(@"install_files\*.*"))
+                }
+            };
+
+            project.BuildMsiCmd();
+        }
+    }
+}
